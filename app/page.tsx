99f4a0ba3cc3b7de4e48bffdbaa687c3aad937ac
Matthew Means <matty_means@outlook.com>
2026-09-06 01:54:50 -0600

    fixed redirect issue

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import {redirect} from "next/navigation";
 
 export default function Home() {
   useEffect(() => {
-    if (process.env.prelaunch=== "true") {
+    if (process.env.NEXT_PUBLIC_PRELAUNCH === "true") {
       redirect('/prelaunch');
     }
   }, []);
